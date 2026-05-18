956e12f15e47daee1020627fd6381b6da1222491
Manuel Hernandez <manu.hdz.dev@gmail.com>
2026-05-18 10:03:05 -0600

    feat(layout): implement main dashboard layout

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { RouterOutlet }
+  from '@angular/router';
 
+import { MatSidenavModule }
+  from '@angular/material/sidenav';
+
+import { Navbar }
+  from '../navbar/navbar';
+
+import { Sidebar }
+  from '../sidebar/sidebar';
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  imports: [
+
+    RouterOutlet,
+
+    MatSidenavModule,
+
+    Navbar,
+
+    Sidebar
+  ],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
+  standalone: true
 })
 export class MainLayout {
 
